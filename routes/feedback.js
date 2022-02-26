0e83d3919a281f585578fbf7084b368acd78a23d
Akshat Kumar <akshatkumar4433@gmail.com>
2022-02-26 18:36:37 -0500

    feedback page working

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const {check, validationResult} = require('express-validator')
 
 //Here this is next for app.use in server.js file
 
@@ -12,10 +12,16 @@ module.exports = (params)=> {
 router.get('/', async (request, response,next) =>{
   try {
     const feedback = await feedbackService.getList()
+    const errors = request.session.feedback ? request.session.feedback.errors:false;
+    const successMessage = request.session.feedback ? request.session.feedback.message:false;
+    request.session.feedback = {};
+
     return response.render('layout', {
         pageTitle: 'Feedback',
         template:'feedback',
         feedback,
+        errors,
+        successMessage,
     });
   }
   catch(error) {
@@ -23,9 +29,44 @@ router.get('/', async (request, response,next) =>{
   }
 });
 
-router.post('/', (request, response) =>{
-  console.log(request.body);
-  return response.send(`feedback form posted`)
+router.post('/', [
+  check('name')
+  .trim()
+  .isLength({min:3})
+  .escape()
+  .withMessage('A name is required'),
+  check('email')
+  .trim()
+  .isEmail()
+  .normalizeEmail()
+  .withMessage('A validate email address'),
+  check('title')
+  .trim()
+  .isLength({min:3})
+  .escape()
+  .withMessage('A title is required'),
+  check('message')
+  .trim()
+  .isLength({min:5})
+  .escape()
+  .withMessage('A message is required'),
+],
+async (request, response) =>{
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    request.session.feedback = {
+      errors: errors.array()
+    };
+    return response.redirect('/feedback');
+  }
+
+   const {name, email, title, message} = request.body;
+   await feedbackService.addEntry(name, email, title, message)
+   request.session.feedback = {
+     message : 'Thank you for your feedback!',
+   }
+
+  return response.redirect('/feedback');
 });
 
  return router;
