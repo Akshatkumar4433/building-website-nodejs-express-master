bee8e65ffc8f78759f5c7a792bbc91a6fd2ca8ec
Akshat Kumar <akshatkumar4433@gmail.com>
2022-03-02 21:41:15 -0500

    Learning api

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,36 +4,11 @@ const {check, validationResult} = require('express-validator')
 
 //Here this is next for app.use in server.js file
 
-module.exports = (params)=> {
-  //params are coming from app.use
-  let feedbackService = params.feedbackService;
-
-
-router.get('/', async (request, response,next) =>{
-  try {
-    const feedback = await feedbackService.getList()
-    const errors = request.session.feedback ? request.session.feedback.errors:false;
-    const successMessage = request.session.feedback ? request.session.feedback.message:false;
-    request.session.feedback = {};
-
-    return response.render('layout', {
-        pageTitle: 'Feedback',
-        template:'feedback',
-        feedback,
-        errors,
-        successMessage,
-    });
-  }
-  catch(error) {
-    return next(error)
-  }
-});
-
-router.post('/', [
+const validations =  [
   check('name')
   .trim()
   .isLength({min:3})
-  .escape()
+  .escape() //no html and js embbed
   .withMessage('A name is required'),
   check('email')
   .trim()
@@ -50,24 +25,76 @@ router.post('/', [
   .isLength({min:5})
   .escape()
   .withMessage('A message is required'),
-],
-async (request, response) =>{
-  const errors = validationResult(request);
-  if (!errors.isEmpty()) {
-    request.session.feedback = {
-      errors: errors.array()
-    };
-    return response.redirect('/feedback');
+];
+
+
+module.exports = (params)=> {
+  //params are coming from app.use
+  let feedbackService = params.feedbackService;
+
+
+router.get('/', async (request, response,next) =>{
+  try {
+    const feedback = await feedbackService.getList()
+    const errors = request.session.feedback ? request.session.feedback.errors:false;
+    const successMessage = request.session.feedback ? request.session.feedback.message:false;
+    request.session.feedback = {};
+
+    return response.render('layout', {
+        pageTitle: 'Feedback',
+        template:'feedback',
+        feedback,
+        errors, //using ejs they are then displayed
+        successMessage,//using ejs they displayed
+    });
   }
-
-   const {name, email, title, message} = request.body;
-   await feedbackService.addEntry(name, email, title, message)
-   request.session.feedback = {
-     message : 'Thank you for your feedback!',
-   }
-
-  return response.redirect('/feedback');
+  catch(error) {
+    return next(error)
+  }
 });
 
+router.post('/', validations,
+async (request, response) =>{
+  try {
+    //it validates based on validation array
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) {
+      //if there are errors
+      //create object for that session (user, browser, phone)
+      //store then as errors property
+      //redirect to /feedback
+      request.session.feedback = {
+        errors: errors.array()
+      };
+      return response.redirect('/feedback');
+    }
+
+    const {name, email, title, message} = request.body;
+    await feedbackService.addEntry(name, email, title, message)
+    request.session.feedback = {
+      message : 'Thank you for your feedback!',
+    }
+
+   return response.redirect('/feedback');
+
+ } catch (e) { return next(err)}
+
+
+});
+  router.post('/api', validations, async (request, response, next) => {
+   try {
+      const errors = validationResult(request);
+      if (!errors.isEmpty()) {
+        return response.json({errors:errors.array()})
+      }
+      const {name, email, title, message} = request.body;
+      await feedbackService.addEntry(name, email, title, message)
+      const feedback = await feedbackService.getList();
+      return response.json({feedback});
+   } catch (err) {
+     return next(err)
+   }
+  });
  return router;
 }
